24558d792d1fa9ce7222fc4e1c5498bff6dee545
Luffy2025 <bestparty2025@gmail.com>
2024-11-24 16:27:50 +0800

    feature: add directory index in http cmd

--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -1,19 +1,25 @@
-use anyhow::Result;
-use axum::extract::{Path, State};
+use anyhow::{anyhow, Result};
+use axum::extract::{Path, Query, State};
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::routing::get;
 use axum::Router;
+use serde::Deserialize;
+use std::fs;
 use std::net::SocketAddr;
 use std::path::PathBuf;
 use std::sync::Arc;
-use tracing::{info, warn};
-
 use tower_http::services::ServeDir;
+use tracing::{info, warn};
 
 #[derive(Debug)]
 struct HttpServeState {
     path: PathBuf,
+}
+
+#[derive(Debug, Deserialize)]
+struct HttpServeParam {
+    deep: Option<u8>,
 }
 
 pub async fn process_http_serve(path: PathBuf, port: u16) -> Result<()> {
@@ -35,12 +41,26 @@ pub async fn process_http_serve(path: PathBuf, port: u16) -> Result<()> {
 async fn file_handler(
     State(state): State<Arc<HttpServeState>>,
     Path(path): Path<String>,
+    Query(params): Query<HttpServeParam>,
 ) -> impl IntoResponse {
     let p = std::path::Path::new(&state.path).join(path);
     info!("Serving file {:?}", p);
 
+    let expect_deep = params.deep.unwrap_or(0);
+
     if !p.exists() {
         (StatusCode::NOT_FOUND, format!("File not found: {:?}", p))
+    } else if p.is_dir() {
+        match tree(p, expect_deep) {
+            Ok(content) => (StatusCode::OK, content),
+            Err(e) => {
+                warn!("Failed to list directory {:?}", e);
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    "Failed to list directory".to_string(),
+                )
+            }
+        }
     } else {
         match tokio::fs::read_to_string(p).await {
             Ok(content) => {
@@ -58,6 +78,68 @@ async fn file_handler(
     }
 }
 
+fn tree(path: PathBuf, expect_deep: u8) -> Result<String> {
+    if !path.is_dir() {
+        return Err(anyhow!("{} is not a directory", path.display()));
+    }
+    let mut buf = String::new();
+    buf.push_str("\n------------------ use std::fs ----------------------\n");
+    // use std::fs;
+    buf.push_str(format_filename(&path, "")?.as_str());
+    walk_dir_with_std(&mut buf, path.clone(), 1, expect_deep)?;
+
+    buf.push_str("\n---------------- use walkdir::WalkDir ----------------\n");
+
+    // use walkdir::WalkDir;
+    walk_dir_with_walkdir(&mut buf, path, expect_deep)?;
+    Ok(buf)
+}
+
+fn walk_dir_with_std(buf: &mut String, path: PathBuf, deep: u8, expect_deep: u8) -> Result<()> {
+    if expect_deep > 0 && deep > expect_deep {
+        return Ok(());
+    }
+
+    if !path.is_dir() {
+        return Ok(());
+    }
+
+    for entry in (fs::read_dir(path)?).flatten() {
+        let path = entry.path();
+        if path.is_dir() {
+            let indent = "  ".repeat(deep as usize);
+            buf.push_str(format_filename(&path, &indent)?.as_str());
+            walk_dir_with_std(buf, entry.path(), deep + 1, expect_deep)?;
+        } else {
+            let indent = "  ".repeat(deep as usize);
+            buf.push_str(format_filename(&path, &indent)?.as_str());
+        }
+    }
+
+    Ok(())
+}
+
+fn walk_dir_with_walkdir(buf: &mut String, path: PathBuf, expect_deep: u8) -> Result<()> {
+    let walker = if expect_deep > 0 {
+        walkdir::WalkDir::new(path).max_depth(expect_deep as _)
+    } else {
+        walkdir::WalkDir::new(path)
+    };
+    for entry in walker.into_iter().flatten() {
+        let indent = "  ".repeat(entry.depth());
+        buf.push_str(format_filename(entry.path(), &indent)?.as_str());
+    }
+    Ok(())
+}
+
+fn format_filename(path: &std::path::Path, indent: &str) -> Result<String> {
+    if let Some(name) = path.file_name() {
+        Ok(format!("{}|--{}\n", indent, name.to_string_lossy()))
+    } else {
+        Err(anyhow!("Failed to get file name"))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -67,10 +149,14 @@ mod tests {
         let state = Arc::new(HttpServeState {
             path: PathBuf::from("."),
         });
-        let (header, _) = file_handler(State(state), Path("Cargo.toml".to_string()))
-            .await
-            .into_response()
-            .into_parts();
+        let (header, _) = file_handler(
+            State(state),
+            Path("Cargo.toml".to_string()),
+            Query(HttpServeParam { deep: None }),
+        )
+        .await
+        .into_response()
+        .into_parts();
         assert_eq!(header.status, StatusCode::OK);
     }
 }
